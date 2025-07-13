ed522d8a95cc1450a73e799a8ffd641287418f95
Henrique Patti <rezendepatti@gmail.com>
2025-07-13 12:17:24 -0300

    adiciona thresholds ao teste post /login

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,6 +3,10 @@ import { sleep, check } from 'k6';
 
 export const options = {
   iterations: 10,
+  thresholds: {
+    http_req_duration: ['p(90)<10', 'max<10'],
+    http_req_failed: ['rate<0.05']
+  }
 };
 
 export default function () {
